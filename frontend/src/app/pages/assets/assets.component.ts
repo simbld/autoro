e2ed8818369e69728a4b00855c6e6ba6a3e42b1d
simbld <s_bullado51@hotmail.com>
2026-03-19 23:59:48 +0100

    Enhance AssetsComponent with new imports and methods for improved functionality

--- frontend/src/app/pages/assets/assets.component.ts
+++ frontend/src/app/pages/assets/assets.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from "@angular/core";
+import {MatCard, MatCardModule} from "@angular/material/card";
+import {interval, Observable, Subscription, switchMap} from "rxjs";
+import {Portfolio, Position} from "../../services/trading.service";
+import {CommonModule} from "@angular/common";
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-assets',
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatCardModule],
   templateUrl: './assets.component.html',
   styleUrl: './assets.component.scss'
 })
-export class AssetsComponent {
 
+export class AssetsComponent {
+    private http = inject(AssetsComponent);
+    private sub!: Subscription;
+    portfolio: Portfolio | null = null;
+    columns = ['instrument', 'direction', 'openRate', 'sl', 'tp', 'tsl', 'close'];
+
+    getRates(): Observable<Position> {
+        return this.http.get<Position>(`${this.api}/positions`);
+    }
 }
