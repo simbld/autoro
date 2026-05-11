afa5c44edbc118d50d9fa111720aff7fd5e4953d
simbld <s_bullado51@hotmail.com>
2026-05-11 23:59:41 +0200

    Update app component imports: remove NgOptimizedImage for cleaner module structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,12 +4,11 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTooltip} from "@angular/material/tooltip";
-import {NgOptimizedImage} from "@angular/common";
 import {PlugComponent} from "./components/plug/plug.component";
 
 @Component({
   selector: 'app-root',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatListModule, MatIconModule, MatTooltip, NgOptimizedImage, PlugComponent],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatListModule, MatIconModule, MatTooltip, PlugComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
