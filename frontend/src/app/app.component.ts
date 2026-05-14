c315f0a87351e8f66996d5d7d2364a8ad852a285
simbld <s_bullado51@hotmail.com>
2026-05-15 00:33:55 +0200

    Update app component imports: add DecimalPipe and MatCard modules for enhanced functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,10 +5,12 @@ import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTooltip} from "@angular/material/tooltip";
 import {PlugComponent} from "./components/plug/plug.component";
+import {DecimalPipe} from "@angular/common";
+import {MatCard, MatCardContent} from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatListModule, MatIconModule, MatTooltip, PlugComponent],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatListModule, MatIconModule, MatTooltip, PlugComponent, DecimalPipe, MatCard, MatCardContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
