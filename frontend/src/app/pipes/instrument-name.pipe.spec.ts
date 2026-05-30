5214288db244b8f7317f6ff14c21762cb721759d
simbld <s_bullado51@hotmail.com>
2026-05-30 23:59:40 +0200

    Remove unused label and balance styles to clean up CSS

---
+++ frontend/src/app/pipes/instrument-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { InstrumentNamePipe } from './instrument-name.pipe';
+
+describe('InstrumentNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new InstrumentNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
