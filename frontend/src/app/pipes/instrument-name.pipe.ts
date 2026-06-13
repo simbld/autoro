1b68c55905a5a6f2032c7dad4d5a62375e9657df
simbld <s_bullado51@hotmail.com>
2026-06-13 23:32:29 +0200

    Add standalone instrument name pipe with mapping for SOL, BTC, and ETH

--- frontend/src/app/pipes/instrument-name.pipe.ts
+++ frontend/src/app/pipes/instrument-name.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'instrumentName'
+  name: 'instrumentName',
+    standalone: true
 })
 export class InstrumentNamePipe implements PipeTransform {
+    private readonly mapping: { [key: number]: string } = {
+        100000: 'SOL',
+        100001: 'BTC',
+        100063: 'ETH'
+    }
 
-  transform(value: number, ...args: unknown[]): unknown {
-    return null;
+    transform(value: number): string {
+    return this.mapping[value];
   }
 
 }
