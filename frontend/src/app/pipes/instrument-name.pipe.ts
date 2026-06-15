f69e3b09a71e20a486b367c831862b370be90f99
simbld <s_bullado51@hotmail.com>
2026-06-15 23:59:01 +0200

    Fix mapping order in InstrumentNamePipe and handle unknown values

--- frontend/src/app/pipes/instrument-name.pipe.ts
+++ frontend/src/app/pipes/instrument-name.pipe.ts
@@ -6,13 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class InstrumentNamePipe implements PipeTransform {
     private readonly mapping: { [key: number]: string } = {
-        100000: 'SOL',
-        100001: 'BTC',
-        100063: 'ETH'
+        100063: 'SOL',
+        100001: 'ETH',
+        100000: 'BTC'
     }
 
     transform(value: number): string {
-    return this.mapping[value];
+    return this.mapping[value] ?? 'Unknown';
   }
 
 }
