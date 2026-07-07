eb0ba4478eb61d0bd0c4465ac0bacccc05381bef
simbld <s_bullado51@hotmail.com>
2026-07-07 23:50:59 +0200

    Add CurrencyPipe and implement total calculation in bottom bar component

--- frontend/src/app/components/bottom-bar/bottom-bar.component.ts
+++ frontend/src/app/components/bottom-bar/bottom-bar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-bottom-bar',
-  imports: [],
+    imports: [
+        CurrencyPipe
+    ],
   templateUrl: './bottom-bar.component.html',
   styleUrl: './bottom-bar.component.scss'
 })
 export class BottomBarComponent {
+    balance = 0.01;
+    invested = 3255.01;
+    profit = 1128.96;
 
+    get total(): number {
+        return this.balance + this.invested + this.profit;
+    }
 }
